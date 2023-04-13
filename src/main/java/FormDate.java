85516cd583eda0b26f2587c67c44be2f2c928a32
Anton Titov <anton.titov97@gmail.com>
2023-04-13 19:38:45 +0300

    post

--- src/main/java/FormDate.java
+++ src/main/java/FormDate.java
@@ -1,6 +1,6 @@
 public class FormDate {
-    public int   birthdayDay;
-    public int  birthdayMonth;
-    public int birthdayYear;
+    public int   day;
+    public int  month;
+    public int year;
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,5 @@ public class Main {
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
         post.subscription = "true";
-
-        FormDate.birthday = new FormDate();
-        birthday.birthday.day = "13";
-        post.birthday.month = "6";
-        post.birthday.year = "1999";
     }
 }
